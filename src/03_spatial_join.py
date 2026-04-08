50782397cdf9008a61686fcd267b2dca3cd017cf
eleanor-koo <ek3466@columbia.edu>
2026-04-08 13:22:02 -0400

    Add spatial join function using GeoPandas

--- src/03_spatial_join.py
+++ src/03_spatial_join.py
@@ -1,1 +1,12 @@
+import geopandas as gpd
 
+def spatial_join(df, boundaries):
+    gdf_points = gpd.GeoDataFrame(
+        df,
+        geometry=gpd.points_from_xy(df.longitude, df.latitude),
+        crs="EPSG:4326"
+    )
+
+    joined = gpd.sjoin(gdf_points, boundaries, how="left", predicate="within")
+
+    return joined
